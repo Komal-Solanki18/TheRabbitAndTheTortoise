c747500e6baf1c47c338f5dc8de44e8a47642474
Komal-Solanki18 <komal.solanki_cs19@gla.ac.in>
2020-10-29 20:54:36 +0530

    Add cases for switch statements in Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -6,7 +6,30 @@ public class Tortoise  extends Thread{
       super("Tortoise");
   }
     public void run() {
-
+        for(int index = 0; index < MILESTONES; index++) {
+            switch (index) {
+                case 0:
+                    System.out.println("The Tortoise has started from the start line!");
+                    break;
+                case 1:
+                    System.out.println("The Tortoise has reached the Cool River!");
+                    break;
+                case 2:
+                    System.out.println("The Tortoise has reached the Mountain Hill!");
+                    break;
+                case 3:
+                    System.out.println("The Tortoise has reached the Big Oak Tree!");
+                    System.out.println("The Tortoise is leading and hence is going to sleep!");
+                    try {
+                        Thread.sleep(18000L);
+                    } catch (InterruptedException e) {
+                        System.err.println("The Tortoise's sleep got interrupted!");
+                    }
+                    break;
+                case 4:
+                    System.out.println("The Tortoise has reached the finish line!");
+                    break;
+            }
     }
-
+    }
 }
